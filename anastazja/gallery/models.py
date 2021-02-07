6cbdc759f128d728224d29ea55ea3730159b5fe2
nikolska <nikolska1995@gmail.com>
2021-02-07 16:11:51 +0100

    add gallery view

--- anastazja/gallery/models.py
+++ anastazja/gallery/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.category
+
+
+class Photo(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='media/gallery/')
+
--- anastazja/gallery/views.py
+++ anastazja/gallery/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Category, Photo
+
+
 def images(request):
-	return render(request, 'gallery/gallery.html')
+	categories = Category.objects.order_by('category')
+	photos = Photo.objects.all()
+	ctx = {
+		'categories': categories,
+		'photos': photos
+	}
+	return render(request, 'gallery/gallery.html', ctx)
